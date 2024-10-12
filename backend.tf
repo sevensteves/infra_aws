fab3267c2d2985646fa853c24e796f3b2289ae87
stefan <sevensteves@proton.me>
2024-10-12 16:16:14 +0000

    Created an s3 bucket to store terraform state.

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "sevensteves-terraform-state-bucket"  #
+    key            = "terraform/state/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "sevensteves-terraform-lock-table"
+  }
+}
